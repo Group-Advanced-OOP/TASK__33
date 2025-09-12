f5a595afbf7b8073bfd0741b032588616f3bc5dd
unknown <Yuusuflaki82@gmail.com>
2025-09-12 16:25:56 +0300

    Commit 2
    I initialize on line 15 group as a new CardLayout to manage switching between panels.
    I create on line 16 group2Members as a JPanel that uses group (CardLayout) as its layout manager.
    I declare on line 17 JPanel card1 by calling createCard() with text 'This is Card One', a button 'Go to Card Two', and target 'Card2'.
    I declare  on line 18 JPanel card2 by calling createCard() with text 'This is Card Two', a button 'Go to Card Three', and target 'Card3'.
    I declare on line 19 JPanel card3 by calling createCard() with text 'This is Card Three', a button 'Go to Card One', and target 'Card1'.

--- CardLayoutExample/src/CardLayoutExample.java
+++ CardLayoutExample/src/CardLayoutExample.java
@@ -12,5 +12,11 @@ public class CardLayoutExample {
         group2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         group2.setSize(400, 300);
         group2.setLocationRelativeTo(null);
+        group = new CardLayout();
+        group2Members = new JPanel(group);
+        JPanel card1 = createCard("This is Card One", "Go to Card Two", "Card2");
+        JPanel card2 = createCard("This is Card Two", "Go to Card Three", "Card3");
+        JPanel card3 = createCard("This is Card Three", "Go to Card One", "Card1");
+
     }
 }
