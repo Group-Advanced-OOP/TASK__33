ea37f688d0d3ca9b78a69ba3596857fb00f57dc5
unknown <Yuusuflaki82@gmail.com>
2025-09-12 16:22:08 +0300

    Commit 1
    I create on line 10 a public constructor CardLayoutExample() { to build the GUI when an object is made.
    I initialize on line 11  group2 as a new JFrame with the title 'CardLayout'.
     I set group2’s on line 12 default close operation to EXIT_ON_CLOSE so the program ends when the window is closed.
    I set the frame on line 13 size to 400 by 300 pixels for a medium-sized window.
     I call on line 14 setLocationRelativeTo(null) so the window appears centered on the screen.

--- CardLayoutExample/src/CardLayoutExample.java
+++ CardLayoutExample/src/CardLayoutExample.java
@@ -6,4 +6,11 @@ public class CardLayoutExample {
     public JFrame group2;
     public JPanel group2Members;
     public CardLayout group;
+
+    public CardLayoutExample() {
+        group2 = new JFrame("CardLayout");
+        group2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        group2.setSize(400, 300);
+        group2.setLocationRelativeTo(null);
+    }
 }
