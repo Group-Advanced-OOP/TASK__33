71264b7cdbef5ed8a4b89f7365adaa7ae5d47d43
unknown <Yuusuflaki82@gmail.com>
2025-09-12 15:52:06 +0300

    Commit 2
    I declare on line 6 a public JFrame named group2, which will serve as the main window.
     I declare on line 7 a public JPanel named group2Members to hold the cards that will be switched.
    I declare on line 8 a public CardLayout named group to manage switching between different panels.

--- CardLayoutExample/src/CardLayoutExample.java
+++ CardLayoutExample/src/CardLayoutExample.java
@@ -3,4 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class CardLayoutExample {
+    public JFrame group2;
+    public JPanel group2Members;
+    public CardLayout group;
 }
